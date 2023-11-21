a0e040faab660ed34d659592c46fa4c8884f8941
MauriceJi <151680594+MauriceJi@users.noreply.github.com>
2023-11-21 13:52:52 -0800

    Update tireliftcode.c

--- tireliftcode.c
+++ tireliftcode.c
@@ -63,3 +63,81 @@ long microsecondsToInches(long microseconds) {
 long microsecondsToCentimeters(long microseconds) {
    return microseconds / 29 / 2;
 }
+
+
+
+
+/////////////////////
+#include <LiquidCrystal_I2C.h>
+
+//Ultra-Sonic Sensor
+const int trigPin = 5;
+const int echoPin = 18;
+//Height Button Save
+const int buttonPin = 17;
+//
+const int gobuttonPin = 16;
+
+int buttonState = 0;
+
+
+//unsigned long previousMillis = 0UL;
+//unsigned long interval = 1000UL;
+
+
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+
+long duration;
+int distanceCm;
+int saveddistanceCM;
+
+LiquidCrystal_I2C lcd(0x27, 20, 4); // I2C address 0x27, 20 column and 4 rows
+
+void setup() {
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
+  
+  Serial.begin(115200); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(buttonPin, INPUT_PULLUP);//Pull up button
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+
+  String distanceCmPrint = String(distanceCm);
+  String saveddistanceCmPrint = String(saveddistanceCM);
+  
+  // Prints the distance in the LCD
+  lcd.setCursor(0, 0);            // move cursor the first row
+  lcd.print("Distance: " + distanceCmPrint);          // print message at the first row
+  lcd.setCursor(0, 1);            // move cursor to the second row
+  lcd.print("Saved Distance: " + saveddistanceCmPrint); // print message at the second row
+
+  //buttonState = digitalRead(buttonPin);
+  if (digitalRead(buttonPin) == LOW) {
+    //When button is pressed
+    saveddistanceCM = distanceCm;
+  } else {
+  }
+
+
+ delay(500);
+ lcd.clear();
+
+}
+
